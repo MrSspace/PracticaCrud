5c7406011f2331f790ff3f9e946a748703e82a0e
Sergio Arango <estudiospersonales01@gmail.com>
2023-02-25 19:24:31 -0500

    Se agrega footer, barra de busqueda y se fixean los valores numericos en los formularios para incluir solo valores positivos

--- src/main/java/com/NaveEspacial/BarrowRule/web/ControladorWeb.java
+++ src/main/java/com/NaveEspacial/BarrowRule/web/ControladorWeb.java
@@ -1,7 +1,9 @@
 package com.NaveEspacial.BarrowRule.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ControladorWeb {
@@ -9,6 +11,11 @@ public class ControladorWeb {
     @GetMapping("/")
     public String inicio() {
         return "index";
+    }
+    
+    @PostMapping("/buscar")
+    public String buscar(String buscado, Model model){
+    return "index";
     }
     
 }
